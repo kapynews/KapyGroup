2508c3a34254b30fa8fc712f2639fc465945512c
Pattira <pattira.umy@gmail.com>
2016-08-29 14:50:38 -0700

    fix the head
    fix the head

--- IdentityTest2/Controllers/ManageController.cs
+++ IdentityTest2/Controllers/ManageController.cs
@@ -375,6 +375,11 @@ namespace IdentityTest2.Controllers
             return View();
         }
 
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+
         #region Helpers
         // Used for XSRF protection when adding external logins
         private const string XsrfKey = "XsrfId";
