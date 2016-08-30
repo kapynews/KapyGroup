6fa083a46ec565c76a0b53e60aa6d9d0adace2ab
Kadidi <mah.kadidia@gmail.com>
2016-08-30 16:52:37 -0700

    modified the HotNews function and added the TestUser Controller [to manage users]

---
+++ IdentityTest2/Controllers/TestUserController.cs
@@ -0,0 +1,36 @@
+﻿using IdentityTest2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityTest2.Controllers
+{
+    public class TestUserController : Controller
+    {
+        // GET: TestUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string AddUser()
+        {
+            ApplicationUser user;
+            ApplicationUserStore Store = new ApplicationUserStore(new ApplicationDbContext());
+            ApplicationUserManager userManager = new ApplicationUserManager(Store);
+            user = new ApplicationUser
+            {
+                UserName = "TestUser",
+                Email = "TestUser@test.com"
+            };
+
+            var result = userManager.Create(user);
+            if (!result.Succeeded)
+            {
+                return result.Errors.First();
+            }
+            return "User Added";
+        }
+    }
+}
